9aca9b8abfe702ced0f8e10e49ae44a4a8bc0216
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-11-02 18:51:49 -0500

    Updated route

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(
-    'postgres://localhost:5444/movie_db',
+    'postgres://localhost:5432/movie_db',
     { logging: false }
 );
 
