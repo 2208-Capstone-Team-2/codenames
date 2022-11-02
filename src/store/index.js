df0346a8b3eb29e8301c53134a3190f6fde908b9
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-11-02 18:47:41 -0500

    Initial Kermit

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+/* 
+Here is where you will configure the store 
+*/ 
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {}
+});
+
+export default store;
