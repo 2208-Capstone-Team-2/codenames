4a680bc01f98c1fe7864d0bb76f81d37abdaa4a4
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-11-02 18:54:28 -0500

    Updated db name

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(
-    'postgres://localhost:5432/movie_db',
+    'postgres://localhost:5432/final_project',
     { logging: false }
 );
 
