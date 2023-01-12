c488a7cc2e1413a941bb6d2c72e4ca82d5672ee0
Rosalie Newman <rosalienewman0@gmail.com>
2023-01-12 12:51:22 -0500

    added body parsers, wrote wordbank model, the colors arent showing right in vscode so maybe broken

--- server/app.js
+++ server/app.js
@@ -1,20 +1,23 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
-const volleyball = require('volleyball')
-const app = express()
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const volleyball = require("volleyball");
+const app = express();
 
 // static middleware
-app.use(express.static(path.join(__dirname, '..','public')))
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(cors())
-app.use(volleyball)
+app.use(cors());
+app.use(volleyball);
 
-app.use("/api", require("./api"))
+app.use("/api", require("./api"));
 
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-module.exports = app;
+// body parsers!!!!
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+module.exports = app;
