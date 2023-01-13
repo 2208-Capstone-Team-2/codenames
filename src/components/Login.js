20250d0e815b7f38eb6e1d14a7117c6559441e38
ocjarman <ocjarman@gmail.com>
2023-01-13 09:29:49 -0500

    players and room logging to firebase. adding routes next for room redirection

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,26 +10,30 @@ import { initializeApp } from "firebase/app";
 import { database, app, auth, firebaseConfig } from "../utils/firebase";
 
 const Login = ({ playerId, playerRef }) => {
-  //   const roomId = useSelector((state) => state.players.roomId);
+  const roomId = useSelector((state) => state.players.roomId);
   const username = useSelector((state) => state.players.username);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
+  let roomRef;
 
   playerRef = ref(database, "players/" + playerId);
+  roomRef = ref(database, "rooms/" + roomId);
 
-  console.log({ playerRef });
   console.log({ playerId });
-  //   const handleRoom = (event) => {
-  //     dispatch(setRoomId(event.target.value));
-  //   };
+  console.log({ playerRef });
+  console.log({ roomRef });
+
+  const handleRoom = (event) => {
+    dispatch(setRoomId(event.target.value));
+  };
   const handleName = (event) => {
     dispatch(setUsername(event.target.value));
-    set(playerRef, { username: event.target.value });
   };
 
-  const playerUpdate = () => {
-    set(playerRef, { username: event.target.value });
-    // set(playerRef, { username: event.target.value });
+  const playerLogin = (e) => {
+    e.preventDefault();
+    set(playerRef, { username, roomId });
+    set(roomRef, { roomId: roomId });
+    //     navigate(`/room/${roomId}`);
   };
 
   //   const navigateToRoom = () => {
@@ -37,7 +41,7 @@ const Login = ({ playerId, playerRef }) => {
   //   };
 
   return (
-    <form onSubmit={playerUpdate}>
+    <form onSubmit={playerLogin}>
       <TextField
         id="outlined-basic"
         label="username"
@@ -45,13 +49,13 @@ const Login = ({ playerId, playerRef }) => {
         placeholder="username"
         onChange={handleName}
       />
-      {/* <TextField
+      <TextField
         id="outlined-basic"
         label="room id"
         variant="outlined"
         placeholder="room number"
         onChange={handleRoom}
-      /> */}
+      />
 
       <Button type="submit" variant="contained">
         Join Room
