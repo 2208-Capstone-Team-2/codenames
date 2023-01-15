72c51c150e692d8cd5438ef38b722d840f2dcdb5
ocjarman <ocjarman@gmail.com>
2023-01-15 14:25:17 -0500

    players joining rooms and disconnecting properly

--- src/components/Lobby/Login.jsx
+++ src/components/Lobby/Login.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setUsername, setRoomId } from "../../store/playerSlice";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import { ref, set } from "firebase/database";
+import {  set } from "firebase/database";
 import { database, auth } from "../../utils/firebase";
 import { useEffect } from "react";
 import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
@@ -13,6 +13,8 @@ import styles from "./Lobby.styles";
 import logo from "../../static/images/logoLight.png"; // Tell Webpack this JS file uses this image
 import HowToPlay from "./HowToPlay.jsx";
 import FAQ from "./FAQ.jsx";
+import { ref, child, push, update } from "firebase/database";
+
 const Login = () => {
   const navigate = useNavigate();
   const roomId = useSelector((state) => state.player.roomId);
@@ -21,11 +23,12 @@ const Login = () => {
   let playerId = useSelector((state) => state.player.playerId);
   let playerRef;
   let roomRef;
+  let playerInRoomRef;
 
+  const player = auth.currentUser
   // references to firebase data
   playerRef = ref(database, "players/" + playerId);
   roomRef = ref(database, "rooms/" + roomId);
-
   // setting user room and name on frontend
   const handleRoom = (event) => {
     dispatch(setRoomId(event.target.value));
@@ -37,8 +40,8 @@ const Login = () => {
   //  moving player into room, setting data on firebase
   const playerLogin = (e) => {
     // e.preventDefault();
-    set(roomRef, { roomId: roomId });
-    set(playerRef, { username, roomId });
+      set(roomRef, {roomId: roomId})
+      set(playerRef, { id: playerId, username, roomId });
     navigate(`/room/${roomId}`);
   };
 
--- src/components/RoomView.jsx
+++ src/components/RoomView.jsx
@@ -3,27 +3,52 @@ import { useDispatch, useSelector } from "react-redux";
 import { setRoomId } from "../store/playerSlice";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { onValue, ref, set } from "firebase/database";
-import { database } from "../utils/firebase";
+import { onValue, ref, set, update } from "firebase/database";
 import { setAllPlayers } from "../store/allPlayersSlice";
+import { database, auth } from "../utils/firebase";
+
 const RoomView = () => {
   // for room nav
   const params = useParams("");
   const roomIdFromParams = params.id;
   setRoomId(roomIdFromParams);
-
+  const player = auth.currentUser
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   // frontend state
   const roomId = useSelector((state) => state.player.roomId);
+  const playerId = useSelector((state) => state.player.playerId);
   const username = useSelector((state) => state.player.username);
   const allPlayers = useSelector((state) => state.allPlayers.allPlayers);
   const [loading, setLoading] = useState(false);
 
+
+
   // firebase room  & players reference
   let roomRef = ref(database, "rooms/" + roomId);
+  let hostRef = ref(database, "rooms/" + roomId + "/host/");
   const allPlayersRef = ref(database, "players/");
+  let playerRef = ref(database, "players/" + playerId);
+
+  
+  function makeSpymaster(e, player, username) {  
+    let redSpymaster;
+    let blueSpymaster;
+    if (e.target.value === 'red') {
+      redSpymaster = {
+        playerId: player.uid,
+        username
+      };
+      update(roomRef, {redSpymaster: redSpymaster});
+    } else if (e.target.value === 'blue'){
+        blueSpymaster = {
+          playerId: player.uid,
+          username
+        };
+      update(roomRef, {blueSpymaster: blueSpymaster});
+    }
+  }
 
   useEffect(() => {
     // on loading page if no room or name, send back to join page
@@ -33,13 +58,14 @@ const RoomView = () => {
       console.log("joined room!");
     }
 
-    // whenever users are added (not working for disconnecting users yet)
+    // whenever users are added (need to change this to when theyre added to ROOM)
+    
     onValue(allPlayersRef, (snapshot) => {
       setLoading(true);
       const data = snapshot.val();
 
-      // moving fb data into array on frontend so easier to work with
       let playersInRoom = [];
+      // whenever a player joins room, push them into room array
       Object.values(data).forEach((player) => {
         if (player.roomId === roomId) {
           playersInRoom.push(player);
@@ -51,7 +77,20 @@ const RoomView = () => {
       setLoading(false);
       console.log("new player!");
     });
+
+     
+
+
+
+    // onValue(roomRef, (snapshot) => {
+    //   const roomData = snapshot.val();
+      // // next steps: 
+      // // make roomInfo redux store to hold all room data!
+      // // dispatch(setRoomInfo(roomInfo));
+    // });
   }, []);
+
+  console.log(allPlayers)
 
   if (loading) return <p>...loading...</p>;
   return (
@@ -60,8 +99,11 @@ const RoomView = () => {
       <br></br>
       players:
       {allPlayers?.map((player) => (
-        <p>{player.username}</p>
+        <p key={player.id}>{player.username}</p>
       ))}
+
+      <button onClick={(e) => {makeSpymaster(e, player, username)}} value="red">red</button>
+      <button onClick={(e) => {makeSpymaster(e, player, username)}} value="blue">blue</button>
     </>
   );
 };
