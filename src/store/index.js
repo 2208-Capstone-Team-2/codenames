8619ea6116f9c63137c4d17de04dada7287f7d56
Heidi <lyu9287@gmail.com>
2023-01-14 19:07:01 -0500

    words in game store

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ const store = configureStore({
   reducer: {
     player: playerReducer,
     allPlayers: allPlayersReducer,
-    wordsInGame: wordsInGameSliceReducer,
+    wordsInGame: wordsInGameReducer,
   },
 });
 
