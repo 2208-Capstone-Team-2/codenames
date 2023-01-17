86d7ad87877c363d9548f387bdbbd8fd1751d43c
Joshowens16 <joshowens16@gmail.com>
2023-01-17 10:51:04 -0800

    joining team1 operatives working

--- src/components/teamBoxes/RedTeamBox.jsx
+++ src/components/teamBoxes/RedTeamBox.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import "./redTeamBox.css";
-import { ref, update } from "firebase/database";
-import { database,  } from "../../utils/firebase";
+import { get, ref, update, set, child } from "firebase/database";
+import { database } from "../../utils/firebase";
 
 const RedTeamBox = () => {
   let playerId = useSelector((state) => state.player.playerId);
@@ -11,19 +11,42 @@ const RedTeamBox = () => {
   const username = useSelector((state) => state.player.username);
 
   playerRef = ref(database, "players/" + playerId);
-  const redSpyRef = ref(database, `rooms/${roomId}/redSpymaster`)
+  const teamOneRef = ref(database, `rooms/${roomId}/team-1/`);
+  const teamTwoRef = ref(database, `rooms/${roomId}/team-2/`);
+  const teamOneOperativesRef = ref(database, `rooms/${roomId}/team-1/operatives/`);
+  const teamOneSpymasterRef = ref(database, `rooms/${roomId}/team-1/spymaster/`);
+
   // On click event for a player to be able to join team-1 team as a operative
   const joinRedOp = () => {
-    update(playerRef, {team: 'team-1', role: "operative"})
-    update(redSpyRef, {players: {
-      id: playerId,
-      username: username
-    }})
+    //Here we want to check if a player is already a spymaster, so that they cannot join both
+    get(teamOneSpymasterRef).then((snapshot)=> {
+      if(snapshot.exists()){
+        const teamOneSpymaster = Object.keys(snapshot.val());
+        if(teamOneSpymaster.includes(playerId)){
+          console.log('cannot join both the spymasters and the operatives')
+        } else {
+          set(child(teamOneOperativesRef, playerId), {playerId, username})
+        }
+      } else {
+        set(child(teamOneOperativesRef, playerId), {playerId, username})
+      }
+    })
   }
 
   // On click event for a player to be able to join the blue team-2 as a spymaster
   const joinRedSpy = () => {
-    update(playerRef, {team: 'team-1', role: "spymaster"})
+    get(teamOneOperativesRef).then((snapshot)=> {
+      if(snapshot.exists()){
+        const teamOneOperatives = Object.keys(snapshot.val());
+        if(teamOneOperatives.includes(playerId)){
+          console.log('cannot join both the spymasters and the operatives')
+        } else{
+          set(child(teamOneSpymasterRef, playerId), {playerId, username})
+        }
+      } else {
+        set(child(teamOneSpymasterRef, playerId), {playerId, username})
+      }
+    })
   }
   return (
     <div className="redBoxCard">
