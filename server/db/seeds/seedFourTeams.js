e71848c8063ddf727bab706822d33b0674fa3b2b
Rosalie Newman <rosalienewman0@gmail.com>
2023-01-20 13:25:18 -0500

    what we have so far

---
+++ server/db/seeds/seedFourTeams.js
@@ -0,0 +1,9 @@
+const { Team } = require('../index');
+
+const seedFourTeams = async () => {
+  // Just create 4 teams
+
+  console.log('DONE SEEDING TEAMS WORDPACK...');
+};
+
+module.exports = seedFourTeams;
