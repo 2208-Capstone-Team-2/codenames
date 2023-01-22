da35592177df99fdd6f1b93e63723ec24eeb91c8
Joshowens16 <joshowens16@gmail.com>
2023-01-21 18:14:50 -0800

    began pseudo coding some api routes

--- server/api/player.js
+++ server/api/player.js
@@ -4,4 +4,13 @@ const router = express.Router();
 // eslint-disable-next-line no-unused-vars
 const { Player, Team, Room } = require('../db');
 
-// Not sure what routes we want here.
+// POST --- once a player 'logins' we post their username, uid, and room to the db
+router.post('/addToRoom', async (req, res, next) => {
+  try {
+    //1. Grab player username, uid, and room # off req.body
+    //2. Put player in db, associate their UID to the PK, and them to the room
+    //3. res.send status and maybe some info
+  } catch (error) {
+    next(error);
+  }
+});
