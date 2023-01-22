8c64b2af5b0ae7648ef14770774206284df546d8
Joshowens16 <joshowens16@gmail.com>
2023-01-21 17:30:24 -0800

    player api and model created

---
+++ server/api/player.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+// eslint-disable-next-line no-unused-vars
+const { Player, Team, Room } = require('../db');
+
+// Not sure what routes we want here.
