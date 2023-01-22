35af2076028d7f631f3e8f15d91aaaeca27b09c1
Joshowens16 <joshowens16@gmail.com>
2023-01-21 17:30:53 -0800

    just ignored an eslint warning

--- server/api/player.js
+++ server/api/player.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// eslint-disable-next-line no-unused-vars
 const router = express.Router();
 // eslint-disable-next-line no-unused-vars
 const { Player, Team, Room } = require('../db');
