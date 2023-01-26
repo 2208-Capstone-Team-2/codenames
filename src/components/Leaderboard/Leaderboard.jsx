a2e067420df92c07f2bc3892e432ad93713bdc04
Joshowens16 <joshowens16@gmail.com>
2023-01-26 09:26:58 -0800

    working with tables but I don't really like tables

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './leaderboard.css';
 const dummyData = [
   {
     username: 'Ben',
@@ -22,11 +23,26 @@ const dummyData = [
   },
 ];
 const Leaderboard = () => {
+  dummyData.sort((a, b) => b.wins - a.wins);
   return (
     <div>
-      {dummyData.map((player) => {
-        return <div></div>;
-      })}
+      LEADERBOARD
+      <table>
+        <thead>
+          <tr>
+            <td>Username</td>
+            <td>Wins</td>
+          </tr>
+        </thead>
+        <tbody>
+          {dummyData.map((player, index) => (
+            <tr key={index}>
+              <td>{player.username}</td>
+              <td>{player.wins}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
