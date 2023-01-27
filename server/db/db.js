8d3f307b7a6db0c9b1f96ae02ff14211de025d9e
Rosalie Newman <rosalienewman0@gmail.com>
2023-01-26 19:33:43 -0500

    changed the db_url  in server db db

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize');
-const DB_NAME = 'codenames';
-
-const db = new Sequelize(`postgres://localhost:5432/${DB_NAME}`, {
+const config = {
   logging: false,
-});
+};
+
+const DB_NAME = 'codenames';
+const URL = `postgres://localhost/${DB_NAME}`;
+
+const db = new Sequelize(process.env.DATABASE_URL || URL, config);
 
 module.exports = db;
