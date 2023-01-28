3a8e4acf7a1730e15f35b3b071f53220d3ede790
Rosalie Newman <rosalienewman0@gmail.com>
2023-01-28 16:07:20 -0500

    signin cleaned up a bit

--- src/components/Room/SignInAnonymously.jsx
+++ src/components/Room/SignInAnonymously.jsx
@@ -8,16 +8,15 @@ function SignInAnonymously() {
     // at this point we need to sign them in anonymously to get their browser's uid
     signInAnonymously(auth)
       .then(() => {
-        // ******* set ondisconnect rules here?
+        // ?
       })
       .catch((error) => {
-        // eslint-disable-next-line no-unused-vars
         const errorCode = error.code;
-        // eslint-disable-next-line no-unused-vars
         const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
       });
   }, []);
-  return <div></div>;
+  return <></>;
 }
 
 export default SignInAnonymously;
