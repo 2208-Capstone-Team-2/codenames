fa2bafe1f3b2f54624651d0458b9215a62164e79
Joshowens16 <joshowens16@gmail.com>
2023-01-29 15:03:26 -0800

    added wins and player seed

---
+++ server/db/seeds/playerSeeds.js
@@ -0,0 +1,65 @@
+const { Player } = require('../index');
+
+const playerSeed = async () => {
+  const dummyData = [
+    {
+      id: 1,
+      username: 'Ben',
+      wins: 112,
+    },
+    {
+      id: 2,
+      username: 'Louis',
+      wins: 52,
+    },
+    {
+      id: 3,
+      username: 'Rue',
+      wins: 56,
+    },
+    {
+      id: 4,
+      username: 'Abby',
+      wins: 56,
+    },
+    {
+      id: 5,
+      username: 'Karat',
+      wins: 72,
+    },
+    {
+      id: 6,
+      username: 'Steve',
+      wins: 6,
+    },
+    {
+      id: 7,
+      username: 'Kolby',
+      wins: 1,
+    },
+    {
+      id: 8,
+      username: 'Rose',
+      wins: 56,
+    },
+    {
+      id: 9,
+      username: 'Olivia',
+      wins: 72,
+    },
+    {
+      id: 10,
+      username: 'Heidi',
+      wins: 156,
+    },
+    {
+      id: 11,
+      username: 'Topher',
+      wins: 80,
+    },
+  ];
+  await Promise.all(dummyData.map((player) => Player.create(player)));
+  console.log('DONE SEEDING PLAYERS..');
+};
+
+module.exports = playerSeed;
