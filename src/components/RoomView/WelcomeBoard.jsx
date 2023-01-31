5ee57c13e4810593d3e21037a26ea81788b6f8a7
Heidi <lyu9287@gmail.com>
2023-01-30 21:01:55 -0500

    delete extra word

--- src/components/RoomView/WelcomeBoard.jsx
+++ src/components/RoomView/WelcomeBoard.jsx
@@ -4,14 +4,13 @@ import { useSelector } from 'react-redux';
 import ResetGame from './ResetGame';
 import AllPlayers from './AllPlayers'
 const WelcomeBoard = () => {
-  const { playerId, username, roomId, isHost } = useSelector((state) => state.player);
+  const { username, roomId } = useSelector((state) => state.player);
   return (
     <>
       <div className="welcomeBoard">
         <h3>Welcome, {username}</h3>
         <h3>Room id: {roomId}</h3>
         <h3>
-          Players:
           <AllPlayers />
         </h3>
         <ResetGame />
