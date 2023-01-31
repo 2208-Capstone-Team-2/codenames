72ebd87903915b34d91d2fa1aba3630de9367072
Joshowens16 <joshowens16@gmail.com>
2023-01-30 18:29:44 -0800

    team

--- server/api/team.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Team routes would go here.
-
-module.exports = router;
---
+++ server/api/team.ts
@@ -0,0 +1,6 @@
+import express, { Router } from 'express';
+const router = Router();
+
+// Team routes would go here.
+
+export default router;
