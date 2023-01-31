9138c5f34890a855f56d2a2f9b80caeb3bc0b820
Rosalie Newman <rosalienewman0@gmail.com>
2023-01-31 13:10:06 -0500

    forgot comma now everything is ok

--- server/db/models/word.ts
+++ server/db/models/word.ts
@@ -1,27 +1,17 @@
 // typescripted by rose!
-import Sequelize  from 'sequelize';
+import Sequelize from 'sequelize';
 import db from '../db';
-import {
-  CreationOptional,
-  InferAttributes,
-  InferCreationAttributes,
-  Model,
-} from "sequelize";
+import { CreationOptional, InferAttributes, InferCreationAttributes, Model } from 'sequelize';
 interface ResponseError extends Error {
   status?: number;
 }
 
 const { STRING, INTEGER } = Sequelize;
 
-export interface WordModel
-  extends Model<
-    InferAttributes<WordModel>
-    InferCreationAttributes<WordModel>
-  > {
+export interface WordModel extends Model<InferAttributes<WordModel>, InferCreationAttributes<WordModel>> {
   id: CreationOptional<number>;
   word: string;
 }
-
 
 const Word = db.define<WordModel>('word', {
   id: {
