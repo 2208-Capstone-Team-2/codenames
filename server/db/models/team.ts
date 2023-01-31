5fa3f6bea33aff960b0791bfdf6c77b60cc2c0ef
Rosalie Newman <rosalienewman0@gmail.com>
2023-01-31 13:12:22 -0500

    team ts done

--- server/db/models/team.ts
+++ server/db/models/team.ts
@@ -1,10 +1,20 @@
 // typescripted by rose!
-import { Sequelize } from 'sequelize';
+import Sequelize from 'sequelize';
 import db from '../db';
+import { CreationOptional, InferAttributes, InferCreationAttributes, Model } from 'sequelize';
 
+interface ResponseError extends Error {
+  status?: number;
+}
+
+const { STRING, INTEGER } = Sequelize;
+
+export interface TeamModel extends Model<InferAttributes<TeamModel>, InferCreationAttributes<TeamModel>> {
+  name: string;
+}
 const Team = db.define('team', {
   name: {
-    type: Sequelize.STRING,
+    type: STRING,
     allowNull: true,
   },
 });
