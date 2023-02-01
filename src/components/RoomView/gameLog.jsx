1eaff138deda627c2f08b214a73492103868ef8f
Rosalie Newman <rosalienewman0@gmail.com>
2023-02-01 13:52:05 -0500

    revert gamelog to how it is on main bc i give up with the capitlzation

--- src/components/RoomView/gameLog.jsx
+++ src/components/RoomView/gameLog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './roomView.css';
 import GuessesRemaining from './GuessesRemaining';
 const GameLog = () => {
