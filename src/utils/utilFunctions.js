de2fbaaf339407f86e6a8ae54d04e97e4efcbc73
Joshowens16 <joshowens16@gmail.com>
2023-02-04 19:55:41 -0800

    converted some mas files

--- src/utils/utilFunctions.js
+++
@@ -1,6 +0,0 @@
-export const isEveryRoleFilled = (teamOneOperatives, teamTwoOperatives, teamOneSpymaster, teamTwoSpymaster) => {
-  if (teamOneOperatives.length > 0 && teamTwoOperatives.length > 0 && teamOneSpymaster && teamTwoSpymaster) {
-    return true;
-  }
-  return false;
-};
