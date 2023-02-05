23291b8d51b6cae2de5e90fa3f3ee538ae84e077
Joshowens16 <joshowens16@gmail.com>
2023-02-04 20:02:04 -0800

    done I think:o

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import store from './store';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
