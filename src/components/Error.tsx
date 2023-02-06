aa1f2f3464246664e2dbc53670076ea91fa815d2
Bingdongsanchi <chrisau418@gmail.com>
2023-02-06 13:12:43 -0800

    rose design 404

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import "./error.css";
 
 function Error() {
   return (
-    <div>
+    <div className='container'>
       <h2>Something went wrong...</h2>
+      <img src='https://raw.githubusercontent.com/2208-Capstone-Team-2/codenames/8665c86ca6998f71951d5feef354f5569268f67b/public/images/black.svg' width='300px'/>
+
       <p>most likely, the room you tried joining does not exist.</p>
     </div>
   );
