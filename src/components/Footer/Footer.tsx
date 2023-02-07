4e8b6fd13e289b61743ee7d9b6c2255b805aca7c
Joshowens16 <joshowens16@gmail.com>
2023-02-07 14:40:25 -0800

    footer more or less done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Facebook, Instagram, Twitter, GitHub } from '@mui/icons-material';
-import { FaCcAmex, FaCcDiscover, FaCcMastercard, FaCcStripe, FaCcVisa, FaPaypal } from 'react-icons/fa';
 import './footer.css';
 function Footer() {
   return (
     <div className="footerContainer">
       <div className="topFooter"></div>
       <div className="bottomFooter">
-        <img src="/images/fsa.png" alt="" />
+        <div className="miscInfo">
+          <p>About Us</p>
+          <p>Contact Us</p>
+          <p>FAQ</p>
+          <p>Terms of Service</p>
+        </div>
+        <div className="groupInfo">
+          <p>© 2023 Codenams | Powered by Full Stack Academy Web Dev - Part Time - 2208 - Capstone Group 2</p>
+        </div>
         <div className="icons">
           <a
             style={{ textDecoration: 'none', color: 'white' }}
@@ -17,7 +24,6 @@ function Footer() {
             <GitHub fontSize="large" />
           </a>
         </div>
-        <p>© 2023 Codenams | Powered by Full Stack Academy Web Dev - Part Time - 2208 - Capstone Group 2</p>
       </div>
     </div>
   );
