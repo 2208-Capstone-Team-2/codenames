4fb05f77905d042949a8affc47f64a2629bbd67e
Joshowens16 <joshowens16@gmail.com>
2023-02-06 19:39:11 -0800

    done for the night

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import styles from './Home.styles';
-
 // Component Imports:
 import FAQ from './FAQ';
 import HowToPlay from './HowToPlay';
 import CreateRoomButton from './CreateRoomButton';
 import SignInAnonymously from '../Room/SignInAnonymously';
-//idk our static folder is so weird and I can't get the image to show
-import './homepage.css'
+// idk our static folder is so weird and I can't get the image to show
+import './homepage.css';
 function Home() {
   return (
-    <div style={styles.sx.HomeContainer}>
-      <div style={styles.sx.LoginContainer}>
+    <div className="homeContainer">
+      <div className="codenames">CODENAMES</div>
+      <div className="loginContainer">
         <img src={'../../static/images/logoLight.png'} alt={''} width="50%" />
         <CreateRoomButton />
       </div>
