aafefaebabe881077d81ed0326fb11e3f7a1645c
Joshowens16 <joshowens16@gmail.com>
2023-02-07 09:00:09 -0800

    how to play in a popover and leaderboard link

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,18 +5,20 @@ import HowToPlay from './HowToPlay';
 import CreateRoomButton from './CreateRoomButton';
 import SignInAnonymously from '../Room/SignInAnonymously';
 // idk our static folder is so weird and I can't get the image to show
+import HomeNav from './HomeNavLinks/HomeNav';
 import './homepage.css';
 function Home() {
   return (
-    <div className="homeContainer">
-      <div className="codenames">CODENAMES</div>
-      <div className="loginContainer">
-        <img src={'../../static/images/logoLight.png'} alt={''} width="50%" />
-        <CreateRoomButton />
+    <div>
+      <HomeNav />
+      <div className="homeContainer">
+        <div className="codenames">CODENAMES</div>
+        <div className="loginContainer">
+          <img src={'../../static/images/logoLight.png'} alt={''} width="50%" />
+          <CreateRoomButton />
+        </div>
+        <SignInAnonymously />
       </div>
-      <HowToPlay />
-      <FAQ />
-      <SignInAnonymously />
     </div>
   );
 }
