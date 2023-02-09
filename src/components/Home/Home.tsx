e39a609ef3c942534f67934fef5bb79a94545277
Rosalie Newman <rosalienewman0@gmail.com>
2023-02-08 22:12:03 -0500

    merge conf

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,10 @@
 import React, { useEffect } from 'react';
 // Component Imports:
 import CreateRoomButton from './CreateRoomButton';
-<<<<<<< HEAD
-import SignInAnonymously from '../Room/SignInAnonymously';
-import HomeNav from './HomeNavLinks/HomeNav';
-import './homepage.css';
-=======
 import SignInAnonymously from '../FirebaseAuth/SignInAnonymously';
 import HomeNav from './HomeNavLinks/HomeNav';
 import './homepage.css';
 //idk our static folder is so weird and I can't get the image to show
->>>>>>> origin/main
 
 function Home() {
   SignInAnonymously();
