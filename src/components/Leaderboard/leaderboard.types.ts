4385adb048dcc989bd2d6a59b2b6b0acdd52166b
Joshowens16 <joshowens16@gmail.com>
2023-02-09 11:25:33 -0800

    working on style rework

--- src/components/Leaderboard/leaderboard.types.ts
+++ src/components/Leaderboard/leaderboard.types.ts
@@ -1,13 +1,13 @@
 export interface Player {
-    id: string;
-    username: string;
-    role: string;
-    wins: number;
-    roomId: string | null;
-    teamId: string | null;
-    createdAt: string;
-    updatedAt: string;
-  }
-  export interface PageClickEvent {
-    selected: number;
-  }
+  id: string;
+  username: string;
+  role: string;
+  wins: number;
+  roomId: string | null;
+  teamId: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+export interface PageClickEvent {
+  selected: number;
+}
