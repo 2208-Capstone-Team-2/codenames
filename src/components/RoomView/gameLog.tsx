0e7d2613cf2ed68997f7c3c53e3d2fb43a69d531
Heidi <lyu9287@gmail.com>
2023-02-10 12:21:27 -0500

    add team info on each clue history

--- src/components/RoomView/gameLog.tsx
+++ src/components/RoomView/gameLog.tsx
@@ -1,43 +1,42 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import './roomView.css';
-import GuessesRemaining from './GuessesRemaining';
-
-interface ClueType {
-    clueString: string;
-    clueNumber: number;
-}
-
-interface GameState {
-    gameHistory: (string | ClueType)[];
-    status: string;
-}
-
+import { RootState } from '../../store';
 const GameLog: React.FC = () => {
-    const game = useSelector<{ game: GameState }, GameState>(
-        (state) => state.game
-    );
-    const gameHistory = game.gameHistory;
-    const gameStatus = game.status;
-
-    return ( 
-    <div className='gameLog'>
-    <h3 className='gameHistoryTitle'>Game History</h3>
-    <div className='scrollContent'>
+  const gameHistory = useSelector((state: RootState) => state.game.gameHistory);
+  const team1Spymaster = useSelector((state: RootState) => state.teamOne.teamOneSpymaster);
+  const team2Spymaster = useSelector((state: RootState) => state.teamTwo.teamTwoSpymaster);
+  const currentSubmitter = useSelector((state: RootState) => state.clues.currentClue.playerSubmitting);
+  return (
+    <div className="gameLog">
+      <h3 className="gameHistoryTitle">Game History</h3>
+      <div className="scrollContent">
         {gameHistory.map((singleHistory, index) => {
-            if (typeof singleHistory === 'object') {
-                return (
-                    <p key={index}>
-                        The clue is {singleHistory.clueString} and you have{' '}
-                        {singleHistory.clueNumber} guess(es)
-                    </p>
-                );
-            } else {
-                return <p key={index}>{singleHistory}</p>;
-            }
-        })}</div>
+          if (typeof singleHistory === 'object' && singleHistory.clueNumber != null) {
+            if (
+              singleHistory.playerSubmitting === currentSubmitter &&
+              singleHistory.playerSubmitting === team1Spymaster?.playerId
+            )
+              return (
+                <p key={index}>
+                  team 1's clue is {singleHistory.clueString} and their operators have {singleHistory.clueNumber + 1}{' '}
+                  guess(es)
+                </p>
+              );
+            if (singleHistory.playerSubmitting === team2Spymaster?.playerId)
+              return (
+                <p key={index}>
+                  team 2's clue is {singleHistory.clueString} and their operators have {singleHistory.clueNumber + 1}{' '}
+                  guess(es)
+                </p>
+              );
+          } else if (typeof singleHistory === 'string') {
+            return <p key={index}>{singleHistory}</p>;
+          }
+        })}
+      </div>
     </div>
-    );
+  );
 };
 
 export default GameLog;
