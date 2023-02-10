e84a01ab0957f0931d9275aad738b257d1001c45
Bingdongsanchi <chrisau418@gmail.com>
2023-02-09 23:23:37 -0800

    first draft

---
+++ src/components/RoomView/CardFlip.js
@@ -0,0 +1,26 @@
+import { ReactDOM } from 'react';
+import React from 'react';
+import { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
+
+const CardFlip = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+      <div>
+        This is the front of the card.
+        <button onClick={handleClick}>Click to flip</button>
+      </div>
+
+      <div>
+        This is the back of the card.
+        <button onClick={handleClick}>Click to flip</button>
+      </div>
+    </ReactCardFlip>
+  );
+};
+ReactDOM.render(<CardFlip />, document.querySelector('#root'));
