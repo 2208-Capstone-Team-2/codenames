f84e5da96acae9e2e753f76b03582a9f404acf2c
Heidi <lyu9287@gmail.com>
2023-02-10 13:03:35 -0500

    is it

--- src/components/RoomView/gameLog.tsx
+++ src/components/RoomView/gameLog.tsx
@@ -2,28 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import './roomView.css';
 import { RootState } from '../../store';
+import { useParams } from 'react-router-dom';
+import { get, ref } from 'firebase/database';
+import { database } from '../../utils/firebase';
+
 const GameLog: React.FC = () => {
   const gameHistory = useSelector((state: RootState) => state.game.gameHistory);
   const team1Spymaster = useSelector((state: RootState) => state.teamOne.teamOneSpymaster);
   const team2Spymaster = useSelector((state: RootState) => state.teamTwo.teamTwoSpymaster);
-  const currentSubmitter = useSelector((state: RootState) => state.clues.currentClue.playerSubmitting);
+
   return (
     <div className="gameLog">
       <h3 className="gameHistoryTitle">Game History</h3>
       <div className="scrollContent">
         {gameHistory.map((singleHistory, index) => {
           if (typeof singleHistory === 'object' && singleHistory.clueNumber != null) {
-            if (
-              singleHistory.playerSubmitting === currentSubmitter &&
-              singleHistory.playerSubmitting === team1Spymaster?.playerId
-            )
+            if (team1Spymaster?.playerId.includes(singleHistory.playerSubmitting))
               return (
                 <p key={index}>
                   team 1's clue is {singleHistory.clueString} and their operators have {singleHistory.clueNumber + 1}{' '}
                   guess(es)
                 </p>
               );
-            if (singleHistory.playerSubmitting === team2Spymaster?.playerId)
+            if (team2Spymaster?.playerId.includes(singleHistory.playerSubmitting))
               return (
                 <p key={index}>
                   team 2's clue is {singleHistory.clueString} and their operators have {singleHistory.clueNumber + 1}{' '}
