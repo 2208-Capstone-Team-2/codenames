8870841bc07f44b6e8deb7713a3967e630987fd1
Rosalie Newman <rosalienewman0@gmail.com>
2023-02-10 17:44:26 -0500

    made error container 100vh and it doesnt look awful. but how to make it responsive?

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import "./error.css";
+import './error.css';
 
 function Error() {
   return (
-  <div className='container'>  
-    <div className='imgContainer'>
-      <h2>You come to the wrong place, mate!</h2>
-      <img src='https://raw.githubusercontent.com/2208-Capstone-Team-2/codenames/8665c86ca6998f71951d5feef354f5569268f67b/public/images/black.svg' width='300px'/>
+    <div className="container">
+      <div className="imgContainer">
+        <h2>You come to the wrong place, mate!</h2>
+        <img src="./images/black.svg" alt="a suspicious looking detective" width="300px" />
 
-      <p>Most likely, the room you tried joining does not exist.</p>
+        <p>Most likely, the room you tried joining does not exist.</p>
+      </div>
     </div>
-  </div>
   );
 }
 
