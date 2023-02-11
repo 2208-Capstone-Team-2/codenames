8b92837a89700031fc6670b7bb83da751887f1e2
Rosalie Newman <rosalienewman0@gmail.com>
2023-02-11 11:03:47 -0500

    wrote as onvalue

--- src/components/RoomView/customHooks/OnChildChangedPlayerRef.ts
+++ src/components/RoomView/customHooks/OnChildChangedPlayerRef.ts
@@ -1,30 +1,28 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 //firebase:
-import { database, auth } from '../../../utils/firebase';
-import { onChildChanged, ref } from 'firebase/database';
+import { database } from '../../../utils/firebase';
+import { onChildChanged, onValue, ref } from 'firebase/database';
 //redux:
 import { useDispatch, useSelector } from 'react-redux';
 import { setTeamIdOnPlayer } from '../../../store/playerSlice';
 import { RootState } from '../../../store/index.js';
 
 function OnChildChangedPlayerRef() {
-  console.log('custom hook!');
   const dispatch = useDispatch();
   const { roomId } = useParams();
   const { playerId } = useSelector((state: RootState) => state.player);
 
   useEffect(() => {
-    const nestedPlayerInRoomRef = ref(database, `rooms/${roomId}/players/`);
+    const nestedPlayerInRoomRef = ref(database, `rooms/${roomId}/players/${playerId}`);
     if (playerId) {
-      onChildChanged(nestedPlayerInRoomRef, async (data) => {
-        console.log(`playerId inside onChild: ${playerId}`);
-        // playerId still === '' at this point, so have to use auth? :/
-        // even when listening for playerId changes...?
-        if (data.key === playerId) {
-          if (data.val().teamId) {
+      onValue(nestedPlayerInRoomRef, async (snapshot) => {
+        if (snapshot.exists()) {
+          console.log(`playerId inside onChild: ${playerId}`);
+          const teamIdFromFB = snapshot.val()?.teamId;
+          if (teamIdFromFB) {
             console.log('setting team id as TEAMID');
-            dispatch(setTeamIdOnPlayer(data.val().teamId));
+            dispatch(setTeamIdOnPlayer(teamIdFromFB));
           } else {
             console.log('setting team id as null');
             dispatch(setTeamIdOnPlayer(null));
@@ -33,6 +31,25 @@ function OnChildChangedPlayerRef() {
       });
     }
   }, [playerId]);
+
+  // using on child:
+  // useEffect(() => {
+  //   const nestedPlayerInRoomRef = ref(database, `rooms/${roomId}/players/`);
+  //   if (playerId) {
+  //     onChildChanged(nestedPlayerInRoomRef, async (data) => {
+  //       console.log(`playerId inside onChild: ${playerId}`);
+  //       if (data.key === playerId) {
+  //         if (data.val().teamId) {
+  //           console.log('setting team id as TEAMID');
+  //           dispatch(setTeamIdOnPlayer(data.val().teamId));
+  //         } else {
+  //           console.log('setting team id as null');
+  //           dispatch(setTeamIdOnPlayer(null));
+  //         }
+  //       }
+  //     });
+  //   }
+  // }, [playerId]);
 }
 
 export default OnChildChangedPlayerRef;
