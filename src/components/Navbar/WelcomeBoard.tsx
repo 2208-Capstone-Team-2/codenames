beef8fd0422f17b8f908b54574be2e26fca4ed6a
Joshowens16 <joshowens16@gmail.com>
2023-02-12 17:14:06 -0800

    copy link is there, needs an alert and navbar needs to be cetnered

--- src/components/Navbar/WelcomeBoard.tsx
+++ src/components/Navbar/WelcomeBoard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../RoomView/card.css';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import LinkToClipboard from '../RoomView/LinkToClipboard';
 const WelcomeBoard = () => {
   const { username, roomId } = useSelector((state: RootState) => state.player);
   return (
@@ -10,7 +11,10 @@ const WelcomeBoard = () => {
       <div className="welcomeBoardItem">
         <img src="/images/topBarLogo.svg" alt="codenames" />
       </div>
-      <p className="welcomeBoardItem">Room id: {roomId}</p>
+      <div className="roomIDAndCopy">
+        <p className="welcomeBoardItem">Room id: {roomId}</p>
+        <LinkToClipboard />
+      </div>
     </div>
   );
 };
--- src/components/RoomView/LinkToClipboard.tsx
+++ src/components/RoomView/LinkToClipboard.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import AddLinkIcon from '@mui/icons-material/AddLink';
+import './roomView.css';
 const LinkToClipboard = () => {
-    const url: string = window.location.href;
-    return (
-        <div>
-            <CopyToClipboard text={url}>
-                <button>
-                    Copy this rooms link!
-                </button>
-            </CopyToClipboard>
-        </div>
-    );
+  const url: string = window.location.href;
+  return (
+    <div className="copyContainer">
+      <CopyToClipboard text={url}>
+        <button>
+          Invite <AddLinkIcon />{' '}
+        </button>
+      </CopyToClipboard>
+    </div>
+  );
 };
 
 export default LinkToClipboard;
