2ee64aea52ed56fbfdaf27ee2e04c46818d5f136
Joshowens16 <joshowens16@gmail.com>
2023-02-14 09:06:32 -0800

    formik seems to be working

--- src/components/Room/UsernameForm.tsx
+++ src/components/Room/UsernameForm.tsx
@@ -28,68 +28,71 @@ function UsernameForm({ handleClose }: UsernameFormProps) {
   // If we have a username in redux slice, start it with that. else, start it with empty string.
   const [inputtedUsername, setInputtedUsername] = useState<string>(username ? username : '');
   const [usernameSubmissionDone, setUsernameSubmissionDone] = useState<boolean>(false);
+  const [creationFailure, setCreationFailure] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      const trimmedInputtedUsername: string = values.username.trim();
+      try {
+        // Update our player's model with this new username
+        const bodyToSubmit: { username: string; roomId: string } = {
+          username: trimmedInputtedUsername,
+          roomId,
+        };
+        await axios.put(`/api/player/${playerId}`, bodyToSubmit);
 
-  const submitHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
+        // Update redux
+        dispatch(setUsername(trimmedInputtedUsername));
 
-    // TODO: Validation - may want to use formik/yup?
-    // Make sure the username they gave isn't empty / made of only white spaces
-    const trimmedInputtedUsername: string = inputtedUsername.trim();
-    if (trimmedInputtedUsername === '') {
-      console.log('cannot have empty username');
-      // stop now! and display error. user needs to resubmit. - Or use formik/yup
-    } else {
-      // Update our player's model with this new username
-      const bodyToSubmit: { username: string; roomId: string } = {
-        username: trimmedInputtedUsername,
-        roomId,
-      };
-      await axios.put(`/api/player/${playerId}`, bodyToSubmit);
+        // Update firebase
+        //// Update the 'outer' player ref
+        const playerRef = ref(database, `players/${playerId}`);
+        update(playerRef, { username: trimmedInputtedUsername, roomId, id: playerId });
 
-      // Update redux
-      dispatch(setUsername(trimmedInputtedUsername));
+        //// Update the nested-in-room player
+        const nestedPlayerRef = ref(database, `rooms/${roomId}/players/${playerId}`);
+        const playersInRoomRef = ref(database, `rooms/${roomId}/players/`);
+        //// If they're the host, put that info there too.
 
-      // Update firebase
-      //// Update the 'outer' player ref
-      const playerRef = ref(database, `players/${playerId}`);
-      update(playerRef, { username: trimmedInputtedUsername, roomId, id: playerId });
+        const hostRef = ref(database, `rooms/${roomId}/host`);
 
-      //// Update the nested-in-room player
-      const nestedPlayerRef = ref(database, `rooms/${roomId}/players/${playerId}`);
-      const playersInRoomRef = ref(database, `rooms/${roomId}/players/`);
-      //// If they're the host, put that info there too.
+        if (isHost) {
+          console.log('is host hit');
+          update(hostRef, { playerId, username: trimmedInputtedUsername });
+          onDisconnect(hostRef).remove();
+          dispatch(setHost({ playerId, username: trimmedInputtedUsername }));
+          set(child(playersInRoomRef, playerId), { playerId, username: trimmedInputtedUsername, isHost: true });
+        } else {
+          set(child(playersInRoomRef, playerId), { playerId, username: trimmedInputtedUsername, isHost: false });
+          // we need this here bc if the user joins after the host is set, they wont get an updated host in their redux
+          get(hostRef).then((snapshot) => {
+            if (snapshot.exists()) {
+              let hostData = snapshot.val();
+              dispatch(setHost(hostData));
+            }
+          });
+        }
 
-      const hostRef = ref(database, `rooms/${roomId}/host`);
+        //// Set what to do on disconnect from firebase:
+        ////// When player DCs, remove the nestedPlayer (the one inside the room).
+        onDisconnect(nestedPlayerRef).remove();
 
-      if (isHost) {
-        console.log('is host hit');
-        update(hostRef, { playerId, username: trimmedInputtedUsername });
-        onDisconnect(hostRef).remove();
-        dispatch(setHost({ playerId, username: trimmedInputtedUsername }));
-        set(child(playersInRoomRef, playerId), { playerId, username: trimmedInputtedUsername, isHost: true });
-      } else {
-        set(child(playersInRoomRef, playerId), { playerId, username: trimmedInputtedUsername, isHost: false });
-        // we need this here bc if the user joins after the host is set, they wont get an updated host in their redux
-        get(hostRef).then((snapshot) => {
-          if (snapshot.exists()) {
-            let hostData = snapshot.val();
-            dispatch(setHost(hostData));
-          }
-        });
+        //// remove the outer player ref
+        onDisconnect(playerRef).remove();
+
+        // Change the piece of state that hides this popup
+        setUsernameSubmissionDone(true);
+        handleClose(false);
+      } catch (error: any) {
+        setCreationFailure(true);
+        setErrorMessage(error.response.data);
       }
-
-      //// Set what to do on disconnect from firebase:
-      ////// When player DCs, remove the nestedPlayer (the one inside the room).
-      onDisconnect(nestedPlayerRef).remove();
-
-      //// remove the outer player ref
-      onDisconnect(playerRef).remove();
-
-      // Change the piece of state that hides this popup
-      setUsernameSubmissionDone(true);
-      handleClose(false);
-    }
-  };
+    },
+  });
   if (!playerId) return <p>loading user form popup...</p>;
   return (
     <div className="wrapper">
@@ -97,22 +100,22 @@ function UsernameForm({ handleClose }: UsernameFormProps) {
         <div className="popupContent">
           <p>Welcome to room {roomId} !</p>
           <p>Enter a username...</p>
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <input
-              value={inputtedUsername}
-              onChange={(event) => {
-                setInputtedUsername(event.target.value);
-              }}
+              name="username"
+              type="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              // error={formik.touched.username && Boolean(formik.errors.username)}
+              // helperText={formik.touched.username && formik.errors.username}
               placeholder="username"
               autoFocus
             ></input>
-            <button type="submit" onClick={submitHandler}>
-              continue
-            </button>
+            <button type="submit">continue</button>
           </form>
         </div>
       )}
-      {usernameSubmissionDone && <div className="popupContent">Welcome, {inputtedUsername}</div>}
     </div>
   );
 }
