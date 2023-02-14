6eb6ad1453bafd94e6f701a93d6de02c9b88be97
Joshowens16 <joshowens16@gmail.com>
2023-02-14 09:56:21 -0800

    font changed

--- src/components/Room/UsernameForm.tsx
+++ src/components/Room/UsernameForm.tsx
@@ -37,62 +37,58 @@ function UsernameForm({ handleClose }: UsernameFormProps) {
     validationSchema: validationSchema,
     onSubmit: async (values) => {
       const trimmedInputtedUsername: string = values.username.trim();
-      try {
-        // Update our player's model with this new username
-        const bodyToSubmit: { username: string; roomId: string } = {
-          username: trimmedInputtedUsername,
-          roomId,
-        };
-        await axios.put(`/api/player/${playerId}`, bodyToSubmit);
+      // Update our player's model with this new username
+      const bodyToSubmit: { username: string; roomId: string } = {
+        username: trimmedInputtedUsername,
+        roomId,
+      };
+      await axios.put(`/api/player/${playerId}`, bodyToSubmit);
 
-        // Update redux
-        dispatch(setUsername(trimmedInputtedUsername));
+      // Update redux
+      dispatch(setUsername(trimmedInputtedUsername));
 
-        // Update firebase
-        //// Update the 'outer' player ref
-        const playerRef = ref(database, `players/${playerId}`);
-        update(playerRef, { username: trimmedInputtedUsername, roomId, id: playerId });
+      // Update firebase
+      //// Update the 'outer' player ref
+      const playerRef = ref(database, `players/${playerId}`);
+      update(playerRef, { username: trimmedInputtedUsername, roomId, id: playerId });
 
-        //// Update the nested-in-room player
-        const nestedPlayerRef = ref(database, `rooms/${roomId}/players/${playerId}`);
-        const playersInRoomRef = ref(database, `rooms/${roomId}/players/`);
-        //// If they're the host, put that info there too.
+      //// Update the nested-in-room player
+      const nestedPlayerRef = ref(database, `rooms/${roomId}/players/${playerId}`);
+      const playersInRoomRef = ref(database, `rooms/${roomId}/players/`);
+      //// If they're the host, put that info there too.
 
-        const hostRef = ref(database, `rooms/${roomId}/host`);
+      const hostRef = ref(database, `rooms/${roomId}/host`);
 
-        if (isHost) {
-          console.log('is host hit');
-          update(hostRef, { playerId, username: trimmedInputtedUsername });
-          onDisconnect(hostRef).remove();
-          dispatch(setHost({ playerId, username: trimmedInputtedUsername }));
-          set(child(playersInRoomRef, playerId), { playerId, username: trimmedInputtedUsername, isHost: true });
-        } else {
-          set(child(playersInRoomRef, playerId), { playerId, username: trimmedInputtedUsername, isHost: false });
-          // we need this here bc if the user joins after the host is set, they wont get an updated host in their redux
-          get(hostRef).then((snapshot) => {
-            if (snapshot.exists()) {
-              let hostData = snapshot.val();
-              dispatch(setHost(hostData));
-            }
-          });
-        }
-
-        //// Set what to do on disconnect from firebase:
-        ////// When player DCs, remove the nestedPlayer (the one inside the room).
-        onDisconnect(nestedPlayerRef).remove();
-
-        //// remove the outer player ref
-        onDisconnect(playerRef).remove();
-
-        // Change the piece of state that hides this popup
-        setUsernameSubmissionDone(true);
-        handleClose(false);
-      } catch (error: any) {
-        setCreationFailure(true);
-        setErrorMessage(error.response.data);
+      if (isHost) {
+        console.log('is host hit');
+        update(hostRef, { playerId, username: trimmedInputtedUsername });
+        onDisconnect(hostRef).remove();
+        dispatch(setHost({ playerId, username: trimmedInputtedUsername }));
+        set(child(playersInRoomRef, playerId), { playerId, username: trimmedInputtedUsername, isHost: true });
+      } else {
+        set(child(playersInRoomRef, playerId), { playerId, username: trimmedInputtedUsername, isHost: false });
+        // we need this here bc if the user joins after the host is set, they wont get an updated host in their redux
+        get(hostRef).then((snapshot) => {
+          if (snapshot.exists()) {
+            let hostData = snapshot.val();
+            dispatch(setHost(hostData));
+          }
+        });
       }
+
+      //// Set what to do on disconnect from firebase:
+      ////// When player DCs, remove the nestedPlayer (the one inside the room).
+      onDisconnect(nestedPlayerRef).remove();
+
+      //// remove the outer player ref
+      onDisconnect(playerRef).remove();
+
+      // Change the piece of state that hides this popup
+      setUsernameSubmissionDone(true);
+      handleClose(false);
     },
   });
+  console.log(formik.errors.username);
   if (!playerId) return <p>loading user form popup...</p>;
   return (
     <div className="wrapper">
@@ -102,16 +98,16 @@ function UsernameForm({ handleClose }: UsernameFormProps) {
           <p>Enter a username...</p>
           <form onSubmit={formik.handleSubmit}>
             <input
+              className="usernameInput"
               name="username"
               type="username"
               value={formik.values.username}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              // error={formik.touched.username && Boolean(formik.errors.username)}
-              // helperText={formik.touched.username && formik.errors.username}
-              placeholder="username"
+              placeholder="Enter Username"
               autoFocus
             ></input>
+            <p className="errorText">{formik.errors.username ? '*Username is required' : ' '}</p>
             <button type="submit">continue</button>
           </form>
         </div>
