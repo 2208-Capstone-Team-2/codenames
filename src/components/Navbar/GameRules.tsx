f8951890d188cfa4046f79c9e08cb75d697599be
ocjarman <ocjarman@gmail.com>
2023-02-14 09:32:17 -0500

    game rules and settings styles updated

--- src/components/Navbar/GameRules.tsx
+++ src/components/Navbar/GameRules.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-
+import './gameRules.css'
 const GameRules = () => {
   return (
-    <div>
+    <div className="gameRules">
       <ul>
-        <li>Each team must have one spymaster and at least one operatives.</li>
+        <li>Each team must have one spymaster and at least one operative.</li>
         <li>
           The spymaster should give an appropriate hint to their operative(s) to help them guess the words that belong
           to their team.
         </li>
         <li>If the operative clicks a bystander or the other teams card, the turn ends.</li>
         <li>
-          If an operative chooses a card that belongs to the other team, the other team the operatives turn ends and the
-          other team is given the point.
+          If an operative chooses a card that belongs to the other team, their turn ends and the
+          other team's card is revealed.
         </li>
-        <li>If the operative clicks the Assassin card, the other team wins, so becareful with your hints!</li>
-        <li>First team to select all their cards wins!</li>
+        <li>If the operative clicks the Assassin card, the other team wins, so be careful with your hints!</li>
+        <li>First team to reveal all their cards wins!</li>
       </ul>
     </div>
   );
