065fcc85050ff1517f33d333d4d4e68ed05139e7
Heidi <lyu9287@gmail.com>
2023-02-14 20:01:24 -0500

    just a simple faq to eliminate dead hyperlinks

--- src/components/Home/FAQ.tsx
+++ src/components/Home/FAQ.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './faq.css';
 const FAQ = () => {
   return (
     <div>
-      <h1>These are the frequently asked questions</h1>
+      <Link to={'/'}>
+        <button className="homeButton">&larr;&nbsp;Home</button>
+      </Link>
+      <div className="faqContainer">
+        <h2 className="commonQuestions">Common Questions</h2>
+        <ul className="ulContainer">
+          <li>Q: What's spectator?</li>
+          <p>
+            A: a spectatr can see the board but not able to make guesses or give clues. This is designed for friends
+            that are late to the game but still wants to participate
+          </p>
+          <li>Q: I am inside a room and the game has started, but I can't see the cards.</li>
+          <p>A: Player must assign themselves a role before the board setup in order to see the board.</p>
+          <br />
+          <li>Q: The timer button is not working.</li>
+          <p>A: Timer function is in our roadmap but unfortunately it is currently unavailable.</p>
+          <br />
+          <li>Q: It takes very long time for the website to.</li>
+          <p>
+            A: This site is using a free hosting service with limited capacities. If you want a more seamless
+            experience, you can play it locally by follow our step by step guide in our github repository.{' '}
+          </p>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/RouterComponent.tsx
+++ src/components/RouterComponent.tsx
@@ -5,6 +5,7 @@ import Footer from './Footer/Footer';
 import Home from './Home/Home';
 import Leaderboard from './Leaderboard/Leaderboard';
 import RoomContainer from './Room/RoomContainer';
+import FAQ from './Home/FAQ';
 const RouterComponent = () => {
   return (
     <>
@@ -15,6 +16,7 @@ const RouterComponent = () => {
         <Route path="/room/:roomId" element={<RoomContainer />} />
         <Route path="/404" element={<Error />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/faq" element={<FAQ />} />
         <Route path="/*" element={<Error />} />
       </Routes>
       {/* <Footer /> */}
