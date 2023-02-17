d9b4dc2fbfc97a5c3041c652af2ab098a7c97a9f
Joshowens16 <joshowens16@gmail.com>
2023-02-16 21:14:51 -0800

    mui alerts for clue validation

---
+++ src/components/RoomView/clue/ClueAlert.tsx
@@ -0,0 +1,36 @@
+// import * as React from 'react';
+// import Stack from '@mui/material/Stack';
+// import Snackbar from '@mui/material/Snackbar';
+// import MuiAlert, { AlertProps } from '@mui/material/Alert';
+
+// // tslint:disable-next-line: no-shadowed-variable
+// const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+//   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+// });
+
+// export default function ClueAlert() {
+//   const [open, setOpen] = React.useState(false);
+
+//   const handleClick = () => {
+//     setOpen(true);
+//   };
+
+//   const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+//     if (reason === 'clickaway') {
+//       return;
+//     }
+
+//     setOpen(false);
+//   };
+
+//   return (
+//     <Stack spacing={2} sx={{ width: '100%' }}>
+//       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+//         <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+//           This is a success message!
+//         </Alert>
+//       </Snackbar>
+//       <Alert severity="error">This is an error message!</Alert>
+//     </Stack>
+//   );
+// }
